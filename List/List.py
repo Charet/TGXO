9df7d0554441600e6cb7a17a9c2820bc5942d21d
yeolll <28512134+YEOLLL@users.noreply.github.com>
2021-12-01 01:44:49 +0800

    修改路径 修改命名

--- List/List.py
+++ List/List.py
@@ -1,15 +1,16 @@
 # -*- coding:utf-8 -*-
-import os
-import time
+from os import listdir
+from config import *
 
 
 def List(bot, message):
     chat_id = message["chat"]["id"]
-    doclist = os.listdir("./posts")
-    doc = 'Document List:\n'
-    if not doclist:
+    doc_list = listdir(posts_path)
+
+    if not doc_list:
         bot.sendMessage(chat_id, "You don't have article.")
     else:
-        for i in doclist:
-            doc = doc + i + '\n'
-        bot.sendMessage(chat_id, text=str(doc))
+        doc_list_text = ''
+        for doc in doc_list:
+            doc_list_text += f'{doc}\n'
+        bot.sendMessage(chat_id, text=doc_list_text)
