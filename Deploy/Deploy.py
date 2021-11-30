5994ce62e415a94f1ca3bc42d56f0d6d33048e9d
Char <2779678916@qq.com>
2021-12-01 03:04:05 +0800

    删除了多余代码块

--- Deploy/Deploy.py
+++ Deploy/Deploy.py
@@ -1,26 +1,4 @@
 # -*- coding:utf-8 -*-
 
 def deploy(bot, message):
-    # root_id = bot.root_id
-    # bot_id = bot.bot_id
-    # author = bot.author
-    # version = bot.version
-    # plugin_dir = bot.plugin_dir
-    # plugin_bridge = bot.plugin_bridge
-    # uptime = bot.uptime
-    # response_times = bot.response_times
-    # response_chats = bot.response_chats
-    # response_users = bot.response_users
-
-    chat_id = message["chat"]["id"]
-    user_id = message["from"]["id"]
-    message_id = message["message_id"]
-
-    message_type = message["message_type"]
-    chat_type = message["chat"]["type"]
-
-    prefix = ""
-    with open(bot.path_converter(bot.plugin_dir + "Deploy/__init__.py"), "r", encoding="utf-8") as init:
-        prefix = init.readline()[1:].strip()
-
-    # Write your plugin code below
+    pass
